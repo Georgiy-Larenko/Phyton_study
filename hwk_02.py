a78cdfe0660392685bc19d159deba577643e6237
Georgiy Larenko <goshalar@gmail.com>
2022-11-30 18:44:56 +0500

    update hwk_02

--- hwk_02.py
+++ hwk_02.py
@@ -82,17 +82,17 @@
 # ##### Задание 5. Реализуйте алгоритм перемешивания списка.
 # Из библиотеки random использовать можно только randint
 
-from random import randint
+# from random import randint
 
 
-def mix_lst(lst):
-    for i in range(len(lst)):
-        new_i = randint(0, len(lst) - 1)
-        lst[i], lst[new_i] = lst[new_i], lst[i]
+# def mix_lst(lst):
+#     for i in range(len(lst)):
+#         new_i = randint(0, len(lst) - 1)
+#         lst[i], lst[new_i] = lst[new_i], lst[i]
 
 
-lst_ = [1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9 ,0]
+# lst_ = [1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9 ,0]
 
-print(f'Начальный массив: {lst_}')   
-mix_lst(lst_)
-print(f'Перемешанный массив: {lst_}')
+# print(f'Начальный массив: {lst_}')   
+# mix_lst(lst_)
+# print(f'Перемешанный массив: {lst_}')
