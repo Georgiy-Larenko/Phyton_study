7703f280cf18c0c08b647df5ae89b0425df33980
Georgiy Larenko <goshalar@gmail.com>
2022-12-06 14:48:16 +0500

    add solution

--- hwk_05_task_01.py
+++ hwk_05_task_01.py
@@ -5,5 +5,6 @@ text = input('Введите текст через пробел: ')
 print(f'Введенный текст: {text}')
 
 find = 'абв'
-result = [i for i in text.split() if find not in i]
+data = text.split()
+result = [i for i in data if find not in i]
 print(f'Результат без слов, содержащих "абв": {" ".join(result)}')
