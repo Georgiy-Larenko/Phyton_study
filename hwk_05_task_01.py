c669fc2bf704ceeb90489e7ba9dbe4fd86138f72
Georgiy Larenko <goshalar@gmail.com>
2022-12-06 17:50:29 +0500

    Update solution + add var 2

--- hwk_05_task_01.py
+++ hwk_05_task_01.py
@@ -8,3 +8,14 @@ find = 'абв'
 data = text.split()
 result = [i for i in data if find not in i]
 print(f'Результат без слов, содержащих "абв": {" ".join(result)}')
+
+
+# var 2
+
+
+def del_words(text):
+    text = list(filter(lambda x: 'абв' not in x, text.split()))
+    return " ".join(text)
+
+result_text = del_words(text)
+print(f'Результат без слов, содержащих "абв": {result_text}')
