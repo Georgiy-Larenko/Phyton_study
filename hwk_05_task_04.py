1734a2dfedb47d5da4fdf04aef91fa4e01140cb9
Georgiy Larenko <goshalar@gmail.com>
2022-12-06 14:45:42 +0500

    Add hwk_05 tasks

---
+++ hwk_05_task_04.py
@@ -0,0 +1,6 @@
+# 4 Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+# aaaasssdddwwwwwwwwwwwweeeffffff ->  4a3s3d9w3w3e6f 
+# 4a3s3d9w3w3e6f-> aaaasssdddwwwwwwwwwwwweeeffffff
