29e917b9482d3adca37ba2c616dfcc6185a4cb1f
Georgiy Larenko <goshalar@gmail.com>
2022-12-07 20:21:11 +0500

    update solution

--- hwk_05_task_05.py
+++ hwk_05_task_05.py
@@ -5,13 +5,29 @@
 # [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
 
 
-lst = [3, 1, 2, 3, 4, 6, 1, 7]
+# var 1.
 
-def ups(lst):
-    result = [lst[0]]
-    for i in lst:
-        if i > max(result):
-            result.append(i)
-    return result
+
+# lst = [1, 5, 2, 3, 4, 6, 1, 7]
+
+# def ups(lst):
+#     result = [lst[0]]
+#     for i in lst:
+#         if i > max(result):
+#             result.append(i)
+#     return result
     
-print(ups(lst))
+# print(ups(lst))
+
+
+# var 2.
+
+num = (int(input("Input start index: ")))
+
+lst = [1, 5, 2, 3, 4, 6, 1, 7]
+
+res = [lst[num - 1]]
+res = [i for i in range(len(lst)) if i > max(res)]
+res.insert(0, lst[num - 1])
+
+print(res)
