4dd8af329e5ada28d7513e72cda24d9ab7e11bf0
Georgiy Larenko <goshalar@gmail.com>
2022-12-16 13:09:15 +0500

    Create hwk_07

--- hwk_03.py
+++ hwk_03.py
@@ -51,19 +51,19 @@
 # Var 2
 
 
-def mult_lst(lst):
-    l = len(lst) // 2 + 1 if len(lst) % 2 != 0 else len(lst) // 2
-    new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
-    print(new_lst)
+# def mult_lst(lst):
+#     l = len(lst) // 2 + 1 if len(lst) % 2 != 0 else len(lst) // 2
+#     new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
+#     print(new_lst)
 
 
-lst = [2, 3, 4, 5, 6]
-mult_lst(lst)
-
-# OR
-
-# lst = list(map(int, input("Введите числа через пробел: ").split()))
+# lst = [2, 3, 4, 5, 6]
 # mult_lst(lst)
+
+# # OR
+
+# # lst = list(map(int, input("Введите числа через пробел: ").split()))
+# # mult_lst(lst)
 
 
 
