d6fa306c63f2a07adcb30a41b817ef04f86854f5
Georgiy-Larenko <112169165+Georgiy-Larenko@users.noreply.github.com>
2022-12-17 17:06:30 +0500

    Delete print_data.py

--- Hwk_07/print_data.py
+++
@@ -1,9 +0,0 @@
-
-def print_data(data):
-    if len(data) > 0:
-        print("Фамилия", "Имя", "Телефон", "Примечание")
-        print("-"*85)
-        for item in data:
-            print(item[0], item[1], item[2], item[3])
-    else:
-        print("Справочник пуст!")
