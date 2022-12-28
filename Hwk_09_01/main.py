d49a8615af91805efe918f45fb5ebb392322a133
Georgiy Larenko <goshalar@gmail.com>
2022-12-28 16:17:58 +0500

    update

--- Hwk_09_01/main.py
+++ Hwk_09_01/main.py
@@ -40,7 +40,8 @@ keyboard.row(telebot.types.InlineKeyboardButton(' ', callback_data = 'no'),
 
 @bot.message_handler(commands=['start'])
 def getMessage(msg):
-    bot.send_message(msg.chat.id, 'Привет, *' + msg.from_user.first_name +'* !\nЭто стандартный калькулятор.\nДумаю в объяснении не нуждается.' + '\U0001f609' + '\nПриятного пользования.' + '\U0001f64f')
+    bot.send_message(msg.chat.id, 'Привет, *' + msg.from_user.first_name +'* !\nЭто стандартный калькулятор.\nДумаю в объяснении не нуждается.' + '\U0001f609' + '\nПриятного пользования.' + '\U0001f64f'
+    + '\nДля отправки лога введите /log.')
     lg.logging.info(f'User * {msg.from_user.first_name} ({msg.from_user.id}) * starting program')
 
     global value
@@ -49,9 +50,15 @@ def getMessage(msg):
     else:
         bot.send_message(msg.from_user.id, value, reply_markup = keyboard)
     lg.logging.info(f'User * {msg.from_user.first_name} ({msg.from_user.id}) * send: {keyboard.row}')   
-    
-    
 
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    doc = open(r'Homework\Hwk_09_01\loglist.log', 'rb')
+    bot.send_document(message.from_user.id, doc)
+    doc.close()
+    
+    
 
 @bot.callback_query_handler(func = lambda call: True)
 
